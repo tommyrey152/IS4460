78e561b667e3a7071d7d25029f939adfbba7d93c
Tommy Reynolds <u1309995@utah.edu>
2024-02-05 13:45:58 -0700

    Misc

--- lab4/my_proj/my_app/views.py
+++ lab4/my_proj/my_app/views.py
@@ -1,7 +1,14 @@
 from django.views import View
 from django.shortcuts import render, redirect
 
+def title_name(the_name: str):
+    return the_name.title()
+
 class HomePageView(View):
     def get(self, request):
+
+        my_name = "TOMMY"
+
+        new_name = title_name(my_name)
      
-        return render(request, 'my_app/index.html',{'hi':'hello world!'})
+        return render(request, 'my_app/index.html',{'hi':'Hello World!', 'name':new_name})
